de3b8dc971b5e9794e17381608622d10ae41e740
arssekal <arssekall@gmail.com>
2025-11-29 12:47:08 +0100

    first commit

---
+++ src/main/java/com/arssekal/AgileManager/AgileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.arssekal.AgileManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arssekal/AgileManager/AgileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arssekal.AgileManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
